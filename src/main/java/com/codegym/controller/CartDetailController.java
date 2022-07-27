4d551a7f00f0d53e3d05d7c3bea9ab0f35d77c67
unknown <huenk105@gmail.com>
2022-07-27 20:52:13 +0700

    get cart detail

--- src/main/java/com/codegym/controller/CartDetailController.java
+++ src/main/java/com/codegym/controller/CartDetailController.java
@@ -1,7 +1,12 @@
 package com.codegym.controller;
 
-import com.codegym.model.CartDetail;
+import com.codegym.model.*;
+import com.codegym.security.userpincal.UserDetailService;
 import com.codegym.service.ICartDetailService;
+import com.codegym.service.ICartService;
+import com.codegym.service.ICustomerService;
+import com.codegym.service.IFoodService;
+import com.codegym.service.impl.CartDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +19,15 @@ import java.util.Optional;
 @RequestMapping("/cartdetails")
 public class CartDetailController {
     @Autowired
-    ICartDetailService cartDetailService;
+    private UserDetailService userDetailService;
+    @Autowired
+    private IFoodService foodService;
+    @Autowired
+    private CartDetailService cartDetailService;
+    @Autowired
+    private ICustomerService customerService;
+    @Autowired
+    private ICartService cartService;
 
     @GetMapping("/{id}")
     public ResponseEntity<?> listCartDetailById (@PathVariable Long id) {
@@ -22,6 +35,27 @@ public class CartDetailController {
         if (!cartDetailOptional.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>(cartDetailOptional.get(), HttpStatus.OK);
+    }
+    @GetMapping("/cart/{food_id}")
+    public ResponseEntity<?> findCartDetailByCartAndFood(@PathVariable Long food_id){
+        AppUser appUser = userDetailService.getCurrentUser();
+        Optional<Customer> customerOptional = customerService.findCustomerByAppUser(appUser);
+        if (!customerOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Customer customer = customerOptional.get();
+        Optional<Cart> cartOptional = cartService.findCartByCustomer(customer);
+        if (!cartOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Cart cart = cartOptional.get();
+        Optional<Food> foodOptional = foodService.findById(food_id);
+        if (!foodOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Food food = foodOptional.get();
+        Optional<CartDetail> cartDetailOptional = cartDetailService.findCartDetailByCartAndFood(cart, food);
         return new ResponseEntity<>(cartDetailOptional.get(), HttpStatus.OK);
     }
 }
